3457c8ceb9eee679ed0616e3d0ebf15986f7b2fa
mesh <mwaimonicas@gmail.com>
2023-09-22 12:03:19 +0300

    BMI calc

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter weight in kilograms: ");
+        float weight = input.nextFloat();
+        System.out.print("Enter height in metres: ");
+        float height = input.nextFloat();
+        float bmi = weight/(height * height);
+        System.out.println("Your BMI is: " + String.format("%.2f", bmi));
+    }
+}
